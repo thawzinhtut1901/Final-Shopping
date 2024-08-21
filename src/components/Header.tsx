eb589daccf35d9db9ef6a3583357310ab306ac43
Thone Waddy Khin <139044080+thawzinhtut1901@users.noreply.github.com>
2024-08-21 04:16:28 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,6 @@ const Header = () => {
           <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>{itemAmount}</div>
         </div>
           
-        <a href="/howtcuwork/terms">Policy</a>
       </div>
       {showSideBar && (
             <SideBar isOpen={showSideBar}  onClose={() => setShowSideBar(false)}/>
